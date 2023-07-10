8163a9dd532c85f30779850d08827d9a3a07d84a
JAAR <joe.aaron.acosta@outlook.com>
2023-07-10 14:42:21 -0400

    Version 0

---
+++ exercise_two_fundamentals.py
@@ -0,0 +1,21 @@
+'''
+>>> JAAR
+>>> 07/10/2023
+>>> Practicing Fundamentals Program 2
+>>> Version 0
+'''
+
+'''
+>>> Asks the user for a range and then generate two random numbers within that range then swap both numbers to ensure all of the following operations occur on the lowest of the two numbers:
+
+1. Determine if they are odd
+2. Calculate the sum of all numbers from the start of the range to the random number.
+3. Print all even numbers from one to the random number.
+4. will then calculate the factorial for the lowest number.
+'''
+def main() :
+    print('Hello World!')
+
+
+if __name__ == '__main__' :
+    main()
