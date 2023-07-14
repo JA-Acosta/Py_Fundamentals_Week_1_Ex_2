b358cc05b03fd2dff7d79f4ecc8bfde80c6e254e
JAAR <joe.aaron.acosta@outlook.com>
2023-07-14 16:33:52 -0400

    Version 2

--- exercise_two_fundamentals.py
+++ exercise_two_fundamentals.py
@@ -1,17 +1,19 @@
 '''
 >>> JAAR
->>> 07/11/2023
+>>> 07/14/2023
 >>> Practicing Fundamentals Program 2
->>> Version 1
+>>> Version 2
 '''
 
 '''
 >>> Asks the user for a range and then generate two random numbers within that range then swap both numbers if needed to ensure all of the following operations occur on the lowest of the two numbers:
 
-1. Determine if they are odd
-2. Calculate the sum of all numbers from the start of the range to the random number.
-3. Print all even numbers from one random number to the other.
-4. will then calculate the factorial for the lowest number.
+1. Determine if the smallest random number is greater than zero.
+2. Determine if they are odd
+3. Calculate the sum of all numbers from the start of the range to the random number.
+4. Calculate the factorial for the lowest number.
+5. Print all even numbers from one random number to the other.
+
 '''
 
 import random
@@ -56,15 +58,15 @@ def main() :
     else :
         print('\tNeither number was odd.')
 
-    factorial = None
-    if random_ints[0] == 0 :
-        factorial = 1
-    elif random_ints[0] > 0:
-        factorial = 1
+    factorial = 1
+    int_type = 'neither positive nor negative'
+    if random_ints[0] > 0:
+        int_type = 'positive'
         for n in range(random_ints[0], 0, -1) :
             factorial *= n
-    else :
-        factorial = 'Complex Number. Sorry'
+    elif random_ints[0] < 0 :
+        int_type = 'negative'
+        factorial = 'The factorial is a complex number.'
     string_of_evens = None
     evens_list = [n for n in range(random_ints[0] + 1, random_ints[1]) if n % 2 == 0]
     if len(evens_list) != 0 :
@@ -75,10 +77,11 @@ def main() :
 
     print(f'''
         The calculations are as follows:
-        
-        \tSum for numbers from {inputted_range[0]} to {random_ints[0]}:\t\t{sum(range(inputted_range[0], random_ints[0] + 1))}
-        \tFactorial of smallest int:\t\t{factorial}
-        \tEvens Between both numbers:\t\t{string_of_evens}
+
+        \tThe number {random_ints[0]} is {int_type}.
+        \tSum for numbers from {inputted_range[0]} to {random_ints[0]}:{sum(range(inputted_range[0], random_ints[0] + 1))}
+        \tFactorial for {random_ints[0]} is:{factorial}
+        \tList of Evens Between {random_ints[0]} and {random_ints[1]} is:{string_of_evens}
         ''')
 
 if __name__ == '__main__' :
